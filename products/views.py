7b61dd5f216658a5c997ffc5ebdf38cea05de007
Ryann Cláudio <ryanncmm2002@gmail.com>
2025-05-10 22:20:06 -0300

    [Feat] Implementacao serializers, url e view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 from django.shortcuts import render
 
 # Create your views here.
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
