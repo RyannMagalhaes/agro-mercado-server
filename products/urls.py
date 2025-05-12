9211ad80f41cb9f00c6033a99b09b4de44d838ab
Ryann Cláudio <ryanncmm2002@gmail.com>
2025-05-12 16:37:30 -0300

    [FIX] erro object of type 'type' has no len() corigido na view get_token

--- products/urls.py
+++ products/urls.py
@@ -1,19 +1,10 @@
 from django.urls import path
 from .views import ProductCreateView, ProductListView, ProductDeleteView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+
 
 urlpatterns = [
     path('products', ProductListView.as_view(), name="product-list"),
     path('products/create', ProductCreateView.as_view(), name="product-create"),
     path('products/delete/<int:pk>', ProductDeleteView.as_view(), name="product-delete"),
 
-    #Autenticação
-    #Utilizando as views genéricas do simplejwt que autenticam com username e password
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
--- server/settings.py
+++ server/settings.py
@@ -136,10 +136,12 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer'
-    ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
